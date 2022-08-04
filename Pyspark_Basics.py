048f743a833523a00f1c0202594ad86d3b9176c9
rajachowdary100 <rajachowdary.polepalli99@outlook.com>
2022-08-05 00:56:05 +0530

    modified the code

--- Pyspark_Basics.py
+++ Pyspark_Basics.py
@@ -1,8 +1,15 @@
 # importing required libraries
 import numpy as np
 import pandas as pd
-import pyspark
 import seaborn as sns
+#import pyspark
+#from pyspark.sql import SparkSession
+#spark = SparkSession.builder.appName('sparkDataframe').getOrCreate()
 
+#Creating a pandas dataframe
 df = sns.load_dataset('tips')
 print(df)
+
+#Creating a spark dataframe
+#sdf = spark.createDataFrame(df)
+#print(sdf.show())
