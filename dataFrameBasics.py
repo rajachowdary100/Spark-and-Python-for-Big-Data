5bf95e4fa09ecd5109de0e4053072e202db253e2
rajachowdary100 <rajachowdary.polepalli99@outlook.com>
2022-08-13 22:29:48 +0530

    renamed the file and modified the code

---
+++ dataFrameBasics.py
@@ -0,0 +1,29 @@
+# importing required libraries
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import pyspark
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName('sparkDataframe').getOrCreate()
+# reading the json file
+# df = spark.read.json("people.json")
+# print(df.show())
+# checking the schema of the dataframe
+# print(df.printSchema())
+
+# checking the columns
+# print(df.columns)
+# checking the summary statistics of the dataframe
+# print(df.describe().show())
+
+# Defining/altering the schema-VERY IMPORTANT FOR LARGER DATASETS
+from pyspark.sql.types import (StructField,StringType,
+                               IntegerType,StructType)
+# True indicates that it can be nullable
+data_schema = [StructField('age',IntegerType(),True),
+               StructField('name',StringType(),True)]
+final_struc = StructType(fields=data_schema)
+df = spark.read.json('people.json',schema=final_struc)
+print(df.show())
+print(df.printSchema())
+
